bef2224826d23e98889c88e6e81a6280cf7c6a4d
Solomon-8 <914573285@qq.com>
2019-02-28 17:16:03 +0800

    添加一个简单的演示demo

---
+++ src/main/java/com/qingyou/lesson/controller/EatingController.java
@@ -0,0 +1,42 @@
+package com.qingyou.lesson.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Solomon
+ * @date 2019/2/28
+ * if you founded any bugs in my code
+ * look at my face
+ * that's a feature
+ * ─ wow ──▌▒█───────────▄▀▒▌───
+ * ────────▌▒▒▀▄───────▄▀▒▒▒▐───
+ * ───────▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐───
+ * ─────▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐───
+ * ───▄▀▒▒▒▒▒▒ such difference ─
+ * ──▐▒▒▒▄▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▒▌──
+ * ──▌▒▒▐▄█▀▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐──
+ * ─▐▒▒▒▒▒▒▒▒▒▒▒▌██▀▒▒▒▒▒▒▒▒▀▄▌─
+ * ─▌▒▀▄██▄▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌─
+ * ─▌▀▐▄█▄█▌▄▒▀▒▒▒▒▒▒░░░░░░▒▒▒▐─
+ * ▐▒▀▐▀▐▀▒▒▄▄▒▄▒▒▒ electrons ▒▌
+ * ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒░░░░░░▒▒▒▐─
+ * ─▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌─
+ * ─▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐──
+ * ──▀ amaze ▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▌──
+ * ────▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀───
+ * ───▐▀▒▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀─────
+ * ──▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▀────────
+ * " "
+ *
+ * 这个是简单的入门controller，通过浏览器输入127.0.0.1:8080/eatWhat就可以得到演示中的"随便吃吃，反正也吃不胖"
+ */
+@RestController
+public class EatingController {
+
+    @GetMapping("/eatWhat")
+    public String eat(){
+        return "随便吃吃，反正也吃不胖。";
+    }
+
+}
